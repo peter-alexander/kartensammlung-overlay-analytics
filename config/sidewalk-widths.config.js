72c226d30091caddc64b0a783ab0f4f8ad1eb218
peter-alexander <15900922+peter-alexander@users.noreply.github.com>
2026-06-01 14:53:34 +0200

    Add sidewalk widths JS config

---
+++ config/sidewalk-widths.config.js
@@ -0,0 +1,62 @@
+export default {
+	method: 'fmzk_guided_sis_multiband_cross_v1',
+	crs: {
+		metric: 'EPSG:31256',
+		pmtiles: 'EPSG:4326'
+	},
+	paths: {
+		workDir: 'work/sidewalk-widths',
+		sisIndex: 'work/sidewalk-widths/sis-index.sqlite',
+		metricOutput: 'work/sidewalk-widths/sidewalk_widths_metric.geojsonseq',
+		wgs84Output: 'work/sidewalk-widths/sidewalk_widths_4326.geojsonseq',
+		pmtilesOutput: 'dist/sidewalk_widths.pmtiles',
+		summary: 'dist/sidewalk_widths_summary.json'
+	},
+	sources: {
+		fmzk: {
+			name: 'FMZK Gehsteig Testausschnitt',
+			kind: 'geojson',
+			url: 'https://fahrrad.lima-city.de/testdata/sidewalk-widths/fmzk_gehsteig.geojson'
+
+			// Finale Quelle, vorerst absichtlich deaktiviert:
+			// url: 'Wien WFS: FMZKVERKEHR2OGD, LAYER = Gehsteig, EPSG:31256, outputFormat=json'
+		},
+		sis: {
+			name: 'SIS Belagsflächen Testausschnitt',
+			kind: 'geojson',
+			url: 'https://fahrrad.lima-city.de/testdata/sidewalk-widths/sis_belag.geojson',
+			typeProperty: 'TYP',
+			allowedTypes: ['GG', 'EE', 'HH']
+
+			// Finale Quelle, vorerst absichtlich deaktiviert:
+			// url: 'SIS_BELAG finale Quelle hier einsetzen'
+		}
+	},
+	measurement: {
+		stepM: 1.0,
+		crossSectionHalfLengthM: 30.0,
+		innerProbeM: 0.08,
+		stationMarginM: 0.5,
+		minWidthM: 0.2,
+		fmzkSisSearchBufferM: 1.0,
+		directionWindowStations: 3,
+		directionOutlierDeg: 45,
+		directionHardOutlierDeg: 70,
+		directionStableSpreadDeg: 25,
+		simplifyToleranceM: 0.2,
+		maxStationGapM: 2.5
+	},
+	widthClasses: [
+		{ id: 0, label: '< 1.2 m', min: null, max: 1.2 },
+		{ id: 1, label: '1.2 m - 1.5 m', min: 1.2, max: 1.5 },
+		{ id: 2, label: '1.5 m - 2.0 m', min: 1.5, max: 2.0 },
+		{ id: 3, label: '2.0 m - 3.0 m', min: 2.0, max: 3.0 },
+		{ id: 4, label: '> 3.0 m', min: 3.0, max: null }
+	],
+	pmtiles: {
+		layerName: 'sidewalk_widths',
+		minZoom: 12,
+		maxZoom: 17,
+		baseZoom: 14
+	}
+};
