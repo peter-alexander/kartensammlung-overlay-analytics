f47e2fab83edef57495ec822b8a7b55a7cc07456
peter-alexander <15900922+peter-alexander@users.noreply.github.com>
2026-06-06 10:59:54 +0200

    Repair sidewalk config

--- config/sidewalk-widths.config.js
+++ config/sidewalk-widths.config.js
@@ -1,10 +1,4 @@
-const B = `https://${process.env.EASYNAME_HTTP_HOST || 'tiles.radlobby.at'}/Gehsteigbreiten/Testdaten`;
-
-export default {
-	method: 'fmzk_guided_sis_multiband_cross_v1',
-	crs: { metric: 'EPSG:31256', pmtiles: 'EPSG:4326' },
-	paths: {
-		workDir: 'work/sidewalk-widths',
-		sisIndex: 'work/sidewalk-widths/sis-index.sqlite',
-		metricOutput: 'work/sidewalk-widths/sidewalk_widths_metric.geojsonseq',
-		wgs84Output: 'work/sidewalk-widths/sidewalk_widths_4326
+const B=`https://${process.env.EASYNAME_HTTP_HOST||'tiles.radlobby.at'}/Gehsteigbreiten/Testdaten`;
+const P='work/sidewalk-widths/';
+const W=[[0,'< 1.2 m',null,1.2],[1,'1.2 m - 1.5 m',1.2,1.5],[2,'1.5 m - 2.0 m',1.5,2],[3,'2.0 m - 3.0 m',2,3],[4,'> 3.0 m',3,null]].map(([id,label,min,max])=>({id,label,min,max}));
+export default {method:'fmzk_guided_sis_multiband_cross_v1',crs:{metric:'EPSG:31256',pmtiles:'EPSG:4326'},paths:{workDir:P
